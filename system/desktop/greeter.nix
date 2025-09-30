c3c0c6bccfa96b28d01bd36f07bc52aadb931990
Muhammad Rashid K <mrashieee@gmail.com>
2025-09-30 19:32:40 +0530

    made a wallpaper for greeter

--- system/desktop/greeter.nix
+++ system/desktop/greeter.nix
@@ -7,7 +7,7 @@
     enable = true;
     settings = {
       default_session = {
-      command = "${pkgs.greetd.tuigreet}/bin/tuigreet --sessions ${config.services.displayManager.sessionData.desktops}/share/xsessions:${config.services.xserver.displayManager.sessionData.desktops}/share/wayland-sessions --remember --remember-user-session";
+      command = "${pkgs.greetd.tuigreet}/bin/tuigreet --background  /home/rashocean/Pictures/Wallpapers/nix-black.jpg --sessions ${config.services.displayManager.sessionData.desktops}/share/xsessions:${config.services.xserver.displayManager.sessionData.desktops}/share/wayland-sessions --remember --remember-user-session";
       user = "greeter";
       };
     };
