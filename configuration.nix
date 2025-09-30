13ba87ed77c2958e8e24ed3985b7559f75c4b571
Muhammad Rashid K <mrashieee@gmail.com>
2025-09-30 13:51:20 +0530

    greetd configuration

--- configuration.nix
+++ configuration.nix
@@ -68,8 +68,17 @@
   services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
+  # services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
+  services.greetd = {
+    enable = true;
+    settings = {
+      default_session = {
+      command = "${pkgs.greetd.tuidreet}/bin/tuigreet --sessions ${config.services.xserver.displayManager.sessionData.desktops}/share/xsessions:${config.services.xserver.displayManager.sessionData.desktops}/share/wayland-sessions --remember --remember-user-session";
+      user = "greeter";
+      };
+    };
+  };
 
   # Configure keymap in X11
   services.xserver.xkb = {
