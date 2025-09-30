c0878b29b4375c1f5f5ce6fd845eee72186f75fa
Muhammad Rashid K <mrashieee@gmail.com>
2025-09-30 17:46:46 +0530

    made services more minimal and pretty

--- system/services.nix
+++ system/services.nix
@@ -1,44 +1,34 @@
 { config, pkgs, ... }:
 
 {
-    # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-    # Configure network proxy if necessary
-    # networking.proxy.default = "http://user:password@proxy:port/";
-    # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+  # Networking & Bluetooth
+  networking.networkmanager.enable = true;
+  hardware.bluetooth.enable = true;
 
-    # Enable networking
-    networking.networkmanager.enable = true;
+  # System services
+  services.dbus.enable = true;
+  security.polkit.enable = true;
+  xdg.portal.enable = true;
+  services.printing.enable = true;
 
-    # Enable bluetooth
-    hardware.bluetooth.enable = true;
+  # Pipewire sound
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # jack.enable = true;      # Uncomment if needed
+    # media-session.enable = true;  # Optional
+  };
 
-    services.dbus.enable = true;
-    security.polkit.enable = true;
-    xdg.portal.enable = true;
+  # Disk management & automount
+  services.udisks2.enable = true;
+  services.udiskie.enable = true;
 
-    # Enable CUPS to print documents.
-    services.printing.enable = true;
-
-    # Enable sound with pipewire.
-    services.pulseaudio.enable = false;
-    security.rtkit.enable = true;
-    services.pipewire = {
-        enable = true;
-        alsa.enable = true;
-        alsa.support32Bit = true;
-        pulse.enable = true;
-        # If you want to use JACK applications, uncomment this
-        #jack.enable = true;
-
-        # use the example session manager (no others are packaged yet so this is enabled by default,
-        # no need to redefine it in your config for now)
-        #media-session.enable = true;
-    };
-
-    # Enable touchpad support (enabled default in most desktopManager).
-    # services.xserver.libinput.enable = true;
-
-    environment.systemPackages = with pkgs; [
+  # Environment/system packages
+  environment.systemPackages = with pkgs; [
     wlroots
     wl-clipboard
     mako
@@ -52,5 +42,6 @@
     waybar
     xdg-desktop-portal
     xdg-desktop-portal-gtk
-    ];
+    udiskie        # GUI for automounting
+  ];
 }
