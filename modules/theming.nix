44d2f8de83ea260f901d7ed735551965fbdb88d5
Muhammad Rashid K <mrashieee@gmail.com>
2025-10-04 12:30:53 +0530

    feat: add stylix theming configuration

--- modules/theming.nix
+++ modules/theming.nix
@@ -1,6 +1,15 @@
 { config, pkgs, ... }:
 
 {
+  # Stylix theming
+  stylix = {
+    enable = true;
+    image = "../assets/wallpapers/guts-armour.jpg";
+    polarity = "dark";
+    targets.gtk.enable = true;
+    targets.qt.enable = true;
+  };
+
   environment.systemPackages = with pkgs; [
     hellwal
   ];
