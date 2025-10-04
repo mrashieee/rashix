014ed63c8e47335a8c3aaa99836679d0568e1170
Muhammad Rashid K <mrashieee@gmail.com>
2025-10-04 15:33:48 +0530

    trying gnome extension manager

--- home/config/gnome/gnome.nix
+++ home/config/gnome/gnome.nix
@@ -1,14 +1,18 @@
 { config, pkgs, ... }:
 
 {
-    programs.gnome.extensions = with pkgs.gnomeExtensions; [
-      user-theme
-      dash-to-dock
-      clipboard-indicator
-      gnome-shell-pomodoro
-      system-monitor
-      panel-osd
-      sound-output-device-chooser
-      appindicator
-    ];
+  home.packages = with pkgs; [
+  gnomeExtensions.gnome-shell-extension-manager
+];
+
+  programs.gnome.extensions = with pkgs.gnomeExtensions; [
+    user-theme                              # gnome-shell-extension-user-theme
+    dash-to-dock                           # gnome-shell-extension-dash-to-dock
+    clipboard-indicator                    # gnome-shell-extension-clipboard-indicator
+    pomodoro                              # gnome-shell-extension-pomodoro (may differ from gnome-shell-pomodoro)
+    system-monitor                        # gnome-shell-extension-system-monitor
+    panel-osd                            # gnome-shell-extension-panel-osd
+    sound-output-device-chooser          # gnome-shell-extension-sound-output-device-chooser
+    appindicator                         # gnome-shell-extension-appindicator
+  ];
 }
