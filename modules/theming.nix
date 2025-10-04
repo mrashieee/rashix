244b62ccd3def1cec6789f8adbb6e3e9c2ccaf30
Muhammad Rashid K <mrashieee@gmail.com>
2025-10-04 15:09:52 +0530

    monochrome now

--- modules/theming.nix
+++ modules/theming.nix
@@ -4,10 +4,35 @@
   # Stylix theming
   stylix = {
     enable = true;
-    image = ./../assets/wallpapers/black-hole.jpg;
+    image = ./../assets/wallpapers/nix-black.jpg;
     polarity = "dark";
     targets.gtk.enable = true;
     targets.qt.enable = true;
+    cursor = {
+      package = pkgs.bibata-cursors;
+      name = "Bibata-Modern-Ice";
+      size = 20;
+    };
+    fonts = {
+      monospace = {
+        package = pkgs.nerd-fonts.jetbrains-mono;
+        name = "JetBrains Mono";
+      };
+      sansSerif = {
+        package = pkgs.montserrat;
+        name = "Montserrat";
+      };
+      serif = {
+        package = pkgs.montserrat;
+        name = "Montserrat";
+      };
+      sizes = {
+        applications = 12;
+        terminal = 15;
+        desktop = 11;
+        popups = 12;
+      };
+    };
   };
 
   environment.systemPackages = with pkgs; [
