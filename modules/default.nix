41267a7f11e076a6f43ab2aff328ade3cd6bdac9
Muhammad Rashid K <mrashieee@gmail.com>
2025-10-04 08:58:15 +0530

    refactor: reorganize and restore bootloader, garbage collector, zone, keymap, and mount configurations

--- modules/default.nix
+++ modules/default.nix
@@ -3,14 +3,14 @@
 {
   imports =
     [
-      ./boot/bootloader.nix
       ./desktop/desktopmanager.nix
       ./desktop/windowmanager.nix
       ./desktop/greeter.nix
-      ./garbage-collector/gc.nix
-      ./intrzone/zone.nix
-      ./keyboard/keymap.nix
-      ./mount/mount.nix
+      ./bootloader.nix
+      ./gc.nix
+      ./zone.nix
+      ./keymap.nix
+      ./mount.nix
       ./services.nix
     ];
 }
