f7c3ed16fac7a96b12da4f6d914d0512499080c3
Muhammad Rashid K <mrashieee@gmail.com>
2025-10-05 09:00:00 +0530

    refactor: update GNOME configuration to streamline package management and remove unnecessary extensions

--- home/config/gnome/gnome.nix
+++ home/config/gnome/gnome.nix
@@ -1,22 +1,10 @@
 { config, pkgs, ... }:
 
 {
-  # Adds the Extension Manager GUI for GNOME Shell
-  home.packages = with pkgs; [
-    gnome-shell-extension-manager
-  ];
-
-  # Enables GNOME Shell in your session
-  programs.gnome.enable = true;
-
   # Installs and activates only extensions that are available in pkgs.gnomeExtensions
-  programs.gnome.extensions = with pkgs.gnomeExtensions; [
-    user-theme
-    dash-to-dock
+  home.packages = with pkgs.gnomeExtensions; [
+    user-themes
     clipboard-indicator
-    pomodoro
-    system-monitor
-    panel-osd
     sound-output-device-chooser
     appindicator
   ];
