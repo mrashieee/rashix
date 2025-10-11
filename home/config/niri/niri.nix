6d61b3779925f0f48f1e83ee7db2f671769c2aee
Muhammad Rashid K <mrashieee@gmail.com>
2025-10-11 09:06:31 +0530

    removed comments

--- home/config/niri/niri.nix
+++ home/config/niri/niri.nix
@@ -1,17 +1,14 @@
 { config, pkgs, lib, ... }:
 
 let
-  # Path to your Niri config file in your repo
   niriConfFile = "${config.home.homeDirectory}/confix/home/config/niri/config";
 in
 
 {
-  # Ensure .config exists
   home.file.".config/.keep" = {
     text = "";
   };
 
-  # Activation script to symlink the Niri config
   home.activation.symlink-niri = lib.hm.dag.entryAfter ["writeBoundary"] ''
     mkdir -p ${config.home.homeDirectory}/.config
     ln -sfn ${niriConfFile} ${config.home.homeDirectory}/.config/niri
