02e53aa999402a045872f0d2ab62df24bde10089
Muhammad Rashid K <mrashieee@gmail.com>
2025-10-11 07:58:12 +0530

    niri and hyprland config symilinking i hope it works

--- home/config/hyprland/hyprland.nix
+++ home/config/hyprland/hyprland.nix
@@ -1,7 +1,7 @@
 { config, pkgs, lib, ... }:
 
 let
-  hyprconfDir = "${config.home.homeDirectory}/confix/home/config/hyprland/hypr";
+  hyprconfDir = "${config.home.homeDirectory}/confix/home/config/hyprland/config";
 in
 
 {
@@ -10,7 +10,6 @@ in
   };
 
   home.activation.symlink-hypr = lib.hm.dag.entryAfter ["writeBoundary"] ''
-    echo "Creating symlink for Hyprland config..."
     mkdir -p ${config.home.homeDirectory}/.config
     ln -sfn ${hyprconfDir} ${config.home.homeDirectory}/.config/hypr
   '';
---
+++ home/config/niri/niri.nix
@@ -0,0 +1,19 @@
+{ config, pkgs, lib, ... }:
+
+let
+  # Path to your Niri config file in your repo
+  niriConfFile = "${config.home.homeDirectory}/confix/home/config/niri/config";
+in
+
+{
+  # Ensure .config exists
+  home.file.".config/.keep" = {
+    text = "";
+  };
+
+  # Activation script to symlink the Niri config
+  home.activation.symlink-niri = lib.hm.dag.entryAfter ["writeBoundary"] ''
+    mkdir -p ${config.home.homeDirectory}/.config/niri
+    ln -sfn ${niriConfFile} ${config.home.homeDirectory}/.config/niri
+  '';
+}
