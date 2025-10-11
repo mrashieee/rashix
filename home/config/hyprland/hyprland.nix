69246bfa156dd10242df0fd35cf8ab754467ab1c
Muhammad Rashid K <mrashieee@gmail.com>
2025-10-11 07:35:06 +0530

    took out the comments cause finally know how to read

--- home/config/hyprland/hyprland.nix
+++ home/config/hyprland/hyprland.nix
@@ -1,17 +1,14 @@
 { config, pkgs, lib, ... }:
 
 let
-  # Path to your cloned Hyprland config repo
   hyprconfDir = "${config.home.homeDirectory}/confix/home/config/hyprland/hypr";
 in
 
 {
-  # Optional: ensure .config exists
   home.file.".config/.keep" = {
     text = "";
   };
 
-  # Create a symlink from your repo to ~/.config/hypr automatically
   home.activation.symlink-hypr = lib.hm.dag.entryAfter ["writeBoundary"] ''
     echo "Creating symlink for Hyprland config..."
     mkdir -p ${config.home.homeDirectory}/.config
