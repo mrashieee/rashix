91fbda5b2e2f668f0dc84158fed76a949e7cf8f0
Muhammad Rashid K <mrashieee@gmail.com>
2025-10-11 10:50:32 +0530

    hmmmm why not greetd

--- modules/desktop/greeter.nix
+++ modules/desktop/greeter.nix
@@ -2,14 +2,14 @@
 
 {
   # Enable the greeter.
-  services.displayManager.sddm.enable = true;
-  # services.greetd = {
-  #  enable = true;
-  #  settings = {
-  #    default_session = {
-  #    command = "${pkgs.greetd.tuigreet}/bin/tuigreet --sessions ${config.services.displayManager.sessionData.desktops}/share/xsessions:${config.services.xserver.displayManager.sessionData.desktops}/share/wayland-sessions --remember --remember-user-session";
- #     user = "greeter";
- #     };
- #   };
- # };
+  # services.displayManager.sddm.enable = true;
+  services.greetd = {
+    enable = true;
+    settings = {
+      default_session = {
+      command = "${pkgs.greetd.tuigreet}/bin/tuigreet --sessions ${config.services.displayManager.sessionData.desktops}/share/xsessions:${config.services.xserver.displayManager.sessionData.desktops}/share/wayland-sessions --remember --remember-user-session";
+      user = "greeter";
+      };
+    };
+  };
 }
