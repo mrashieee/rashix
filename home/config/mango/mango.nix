24198240efeda3322a0247e45ac37e83bd487e6f
Muhammad Rashid K <mrashieee@gmail.com>
2025-10-19 11:27:46 +0530

    Added mango to home manager YIPPEEEEEEEEEEE

---
+++ home/config/mango/mango.nix
@@ -0,0 +1,16 @@
+{ config, pkgs, lib, ... }:
+
+let
+  mangoconfDir = "${config.home.homeDirectory}/confix/home/config/mango/config";
+in
+
+{
+  home.file.".config/.keep" = {
+    text = "";
+  };
+
+  home.activation.symlink-hypr = lib.hm.dag.entryAfter ["writeBoundary"] ''
+    mkdir -p ${config.home.homeDirectory}/.config
+    ln -sfn ${mangoconfDir} ${config.home.homeDirectory}/.config/mango
+  '';
+}
