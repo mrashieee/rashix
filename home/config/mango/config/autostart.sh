87eef2e1f59cb695eac2c3231d5cc6f64698d805
Muhammad Rashid K <mrashieee@gmail.com>
2025-10-19 15:10:49 +0530

    some autostart scripts didnt work

--- home/config/mango/config/autostart.sh
+++ home/config/mango/config/autostart.sh
@@ -1,14 +1,3 @@
-#!/bin/bash
-
-set +e
+#!/usr/bin/env bash
 
 waybar
-
-# obs
-dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=wlroots
-
-# keep clipboard content
-wl-clip-persist --clipboard regular --reconnect-tries 0 &
-
-# clipboard content manager
-wl-paste --type text --watch cliphist store & 
