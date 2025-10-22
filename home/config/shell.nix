c69f5a7e47fef4b42ea91861b37d85eed275221e
Muhammad Rashid K <mrashieee@gmail.com>
2025-10-22 16:51:34 +0530

    checking if it works

--- home/config/shell.nix
+++ home/config/shell.nix
@@ -15,4 +15,9 @@
       # export PS1="\[\e[38;5;75m\]\u@\h \[\e[38;5;113m\]\w \[\e[38;5;189m\]\$ \[\e[0m\]"
     '';
   };
+  home.sessionPath = [ "$HOME/.local/bin" ];
+  home.file.".local/bin/scripts" = {
+    source = ./bin;
+    executable = true;
+  };
 }
