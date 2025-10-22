25fa28744dd9dc96e0de606fedf9cc5d11d67d8c
Muhammad Rashid K <mrashieee@gmail.com>
2025-10-22 17:49:43 +0530

    it wasnt working

--- home/config/shell.nix
+++ home/config/shell.nix
@@ -1,10 +1,5 @@
 { config, pkgs, lib, ... }:
 
-let
-  binPath = "${config.home.homeDirectory}/.local/bin";
-  scriptSource = ../bin; # adjust this path if needed
-in
-
 {
   programs.bash = {
     enable = true;
@@ -19,12 +14,11 @@ in
       # export PS1="\[\e[38;5;75m\]\u@\h \[\e[38;5;113m\]\w \[\e[38;5;189m\]\$ \[\e[0m\]"
     '';
   };
-  home.sessionPath = [ binPath ];
-  home.activation.installScripts = lib.hm.dag.entryAfter ["writeBoundary"] ''
-    echo "[home.activation] Installing scripts to ${binPath}..."
-
-    mkdir -p ${binPath}
-    cp -r --no-preserve=mode,ownership ${scriptSource}/* ${binPath}
-    chmod +x ${binPath}/*
+  home.sessionPath = [ "${config.home.homeDirectory}/.local/bin" ];
+  home.file.".local/bin" = {
+    source = ../bin;
+  };
+  home.activation.makeScriptsExecutable = lib.hm.dag.entryAfter ["writeBoundary"] ''
+    chmod +x ${config.home.homeDirectory}/.local/bin/*
   '';
 }
