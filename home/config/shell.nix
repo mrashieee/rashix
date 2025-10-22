4c05621b2f6c75c07cdf8110c4ece0505ef84b82
Muhammad Rashid K <mrashieee@gmail.com>
2025-10-22 17:42:56 +0530

    trying to make it executable

--- home/config/shell.nix
+++ home/config/shell.nix
@@ -15,9 +15,11 @@
       # export PS1="\[\e[38;5;75m\]\u@\h \[\e[38;5;113m\]\w \[\e[38;5;189m\]\$ \[\e[0m\]"
     '';
   };
-  home.sessionPath = [ "$HOME/.local/bin" ];
+  home.sessionPath = [ "${config.home.homeDirectory}/.local/bin" ];
   home.file.".local/bin" = {
     source = ../bin;
-    executable = true;
   };
+  home.activation.makeScriptsExecutable = lib.hm.dag.entryAfter ["writeBoundary"] ''
+    chmod +x ${config.home.homeDirectory}/.local/bin/*
+  '';
 }
