5bc0ba75b45d9d1bb18d12e1ef9caccc7c56c445
Muhammad Rashid K <mrashieee@gmail.com>
2025-10-22 17:48:33 +0530

    trying new cmd

--- home/config/shell.nix
+++ home/config/shell.nix
@@ -1,5 +1,9 @@
 { config, pkgs, lib, ... }:
 
+let
+  binPath = "${config.home.homeDirectory}/.local/bin";
+  scriptSource = ../bin; # adjust this path if needed
+in
 
 {
   programs.bash = {
@@ -15,11 +19,12 @@
       # export PS1="\[\e[38;5;75m\]\u@\h \[\e[38;5;113m\]\w \[\e[38;5;189m\]\$ \[\e[0m\]"
     '';
   };
-  home.sessionPath = [ "${config.home.homeDirectory}/.local/bin" ];
-  home.file.".local/bin" = {
-    source = ../bin;
-  };
-  home.activation.makeScriptsExecutable = lib.hm.dag.entryAfter ["writeBoundary"] ''
-    chmod +x ${config.home.homeDirectory}/.local/bin/*
+  home.sessionPath = [ binPath ];
+  home.activation.installScripts = lib.hm.dag.entryAfter ["writeBoundary"] ''
+    echo "[home.activation] Installing scripts to ${binPath}..."
+
+    mkdir -p ${binPath}
+    cp -r --no-preserve=mode,ownership ${scriptSource}/* ${binPath}
+    chmod +x ${binPath}/*
   '';
 }
