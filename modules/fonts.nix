76c4386fe37d21b1647ec8966e897b6bf0e70ada
Muhammad Rashid K <mrashieee@gmail.com>
2025-12-07 18:22:14 +0530

    updating configd and times new roman

--- modules/fonts.nix
+++ modules/fonts.nix
@@ -4,19 +4,21 @@
   # Fonts
   fonts = {
     packages = with pkgs; [
-    maple-mono.NF-CN
-    noto-fonts
-    noto-fonts-color-emoji
-    noto-fonts-cjk-sans
-    nerd-fonts.jetbrains-mono
-    nerd-fonts.fira-code
-    nerd-fonts.droid-sans-mono
-    font-awesome
-    material-symbols
-  ];
-  fontconfig = {
-    defaultFonts = {
-      emoji = [ "Noto Color Emoji" ]; # or use "OpenMoji Color"
+      corefonts
+      vista-fonts
+      maple-mono.NF-CN
+      noto-fonts
+      noto-fonts-color-emoji
+      noto-fonts-cjk-sans
+      nerd-fonts.jetbrains-mono
+      nerd-fonts.fira-code
+      nerd-fonts.droid-sans-mono
+      font-awesome
+      material-symbols
+    ];
+    fontconfig = {
+      defaultFonts = {
+        emoji = [ "Noto Color Emoji" ]; # or use "OpenMoji Color"
       };
     };
   };
