e23f709cd13593a97d0148932827e5e3f798e94f
Muhammad Rashid K <mrashieee@gmail.com>
2026-03-14 08:58:27 +0530

    trying a shell command

--- home/config/shell.nix
+++ home/config/shell.nix
@@ -10,6 +10,15 @@
       # Run fastfetch
       ${pkgs.fastfetch}/bin/fastfetch
 
+      # Aliases with arguments
+      rashixupdate () {
+        DIR = $1
+        HOST = $(hostname)
+
+        nix flake update "$DIR"
+        sudo nixos-rebuild switch --flake "$DIR#$HOST"
+      }
+
       # Set Prompt
       # export PS1="\[\e[38;5;75m\]\u@\h \[\e[38;5;113m\]\w \[\e[38;5;189m\]\$ \[\e[0m\]"
     '';
