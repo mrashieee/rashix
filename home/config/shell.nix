45d24f60e75dc2ac51cd93bab67996e5c1e77c93
Muhammad Rashid K <mrashieee@gmail.com>
2026-03-21 16:59:02 +0530

    Trying to make fastfetch only appear in first terminal

--- home/config/shell.nix
+++ home/config/shell.nix
@@ -8,8 +8,11 @@
     };
     initExtra = ''
       # Run fastfetch
-      ${pkgs.fastfetch}/bin/fastfetch
-
+      if [[ -z "$FASTFETCH_SHOWN" && -z "$IN_NIX_SHELL" ]]; then
+        ${pkgs.fastfetch}/bin/fastfetch
+        export FASTFETCH_SHOWN=1
+      fi
+      
       # Set Prompt
       # export PS1="\[\e[38;5;75m\]\u@\h \[\e[38;5;113m\]\w \[\e[38;5;189m\]\$ \[\e[0m\]"
     '';
